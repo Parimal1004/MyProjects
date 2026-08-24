b5fec766ab439db50d9d31fc8d1e7e461c1407cb
Parimal Goud Moola <moolaparimal2006@gmail.com>
2026-08-24 22:50:39 +0530

    minor changes

--- server.js
+++ server.js
@@ -283,6 +283,8 @@ app.post("/submit/:subject", (req, res) => {
 
 
 // ================= SERVER =================
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
